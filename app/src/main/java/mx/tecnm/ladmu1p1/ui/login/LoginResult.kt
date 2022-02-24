d580e0a9eb5c5edadaf7f2d002e7f6c0d0e21c8a
Alexis Torres Acosta <altorresac@ittepic.edu.mx>
2022-02-23 18:10:27 -0700

    Initial commit

---
+++ app/src/main/java/mx/tecnm/ladmu1p1/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package mx.tecnm.ladmu1p1.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
